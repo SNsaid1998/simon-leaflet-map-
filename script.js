18c5e1e14980e5418d80137af829cbd022153308
SNsaid1998 <61479527+SNsaid1998@users.noreply.github.com>
2021-02-03 18:18:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
- require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-90, 38.85] // longitude, latitude
-        });
-      });
+L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+	maxZoom: 20,
+	attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+var marker1 = L.marker([51.5, -0.09]).addTo(mymap);
+var marker2 = L.marker([51.5, -0.15]).addTo(mymap);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'green',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
+
+marker1.bindPopup("<b>Hello world!</b><br>I am a popup 1.").openPopup();
+marker2.bindPopup("<b>Hello world!</b><br>I am a popup 2.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
